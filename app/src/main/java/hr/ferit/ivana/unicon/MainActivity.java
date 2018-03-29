36a562b483bedcdc1a02e3553659625d10d96404
DESKTOP-HMFH6EA\ivana <ivana.pristavnik@gmail.com>
2018-03-29 10:29:02 +0200

    Add butterknife, bind views

--- app/src/main/java/hr/ferit/ivana/unicon/MainActivity.java
+++ app/src/main/java/hr/ferit/ivana/unicon/MainActivity.java
@@ -1,13 +1,46 @@
 package hr.ferit.ivana.unicon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bWeight) Button bWeight;
+    @BindView(R.id.bDistance) Button bDistance;
+    @BindView(R.id.bSpeed) Button bSpeed;
+    @BindView(R.id.bVolume) Button bVolume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bWeight)
+    public void showWeightConverter(){
+        
+    }
+
+    @OnClick(R.id.bVolume)
+    public void showVolumeConverter(){
+
+    }
+
+    @OnClick(R.id.bSpeed)
+    public void showSpeedConverter() {
+
+    }
+
+    @OnClick(R.id.bDistance)
+    public void showDistanceConverter(){
+
     }
 }
